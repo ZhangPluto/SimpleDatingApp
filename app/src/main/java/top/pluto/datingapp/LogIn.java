248c2f1fd8788b67125531187a224c98e46ef674
ZhangPluto <lionelzhang99@gmail.com>
2021-03-17 23:31:08 -0400

    Create log in page

---
+++ app/src/main/java/top/pluto/datingapp/LogIn.java
@@ -0,0 +1,14 @@
+package top.pluto.datingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
